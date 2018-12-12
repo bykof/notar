aa0ade9b82768c1473dae991e229c2c9c80b0843
Michael Bykovski <m.bykovski@s-v.de>
2018-12-12 09:23:35 +0100

    Implement  contractModel and createContract

--- contracts/contractModel.js
+++ contracts/contractModel.js
@@ -1,7 +1,27 @@
 import AWS from "aws-sdk";
+import uuid from "uuid";
 
 export default class ContractModel {
     constructor() {
         this.documentClient = new AWS.DynamoDB.DocumentClient();
+    }
+
+    create(userId, hash, contractPdf) {
+
+        return this.documentClient.put(
+            {
+                TableName: 'contracts',
+                Item: {
+                    contractId: uuid.v1(),
+                    createdByUserId: userId,
+                    created: Date.now(),
+                    users: {
+                        [userId]: hash,
+                    },
+                    hash: null,
+                    contractPdf: contractPdf,
+                },
+            }
+        ).promise();
     }
 }
--- contracts/create.js
+++ contracts/create.js
@@ -1,29 +1,20 @@
-import AWS from 'aws-sdk';
-import uuid from 'uuid';
-
-
 import {success, failure} from "../libs/response-lib";
 import hashGenerator from "../keys/hashGenerator";
+import ContractModel from "./contractModel";
 
 
 export async function main(event, context) {
     const body = JSON.parse(event.body);
-    const hash = hashGenerator(body.firstName, body.lastName, body.birthday, body.pin);
+    const hash = hashGenerator(body.firstName, body.lastName, body.birthdayTimestamp, body.pin);
 
-    const documentClient = new AWS.DynamoDB.DocumentClient();
-    const params = {
-        TableName: 'contracts',
-        Item: {
-            'contractId': uuid.v1(),
-            'createdByUserId': event.requestContext.identity.cognitoIdentityId,
-            'created': Date.now(),
-            'active': true
-        },
-    };
 
     try {
-        await documentClient.put(params).promise();
-        return success(params.Item);
+        let result = await new ContractModel().create(
+            event.requestContext.identity.cognitoIdentityId,
+            hash,
+            body.contractPdf,
+        );
+        return success(result.Item);
     } catch (error) {
         return failure({status: false, error: error});
     }
